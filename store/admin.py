d7b159bcf4ae2ebbdb64a2c3d103039875dfbdbd
Burnaviour <meharmuzafar760@gmail.com>
2023-09-01 16:46:31 +0500

    added Product list display

--- store/admin.py
+++ store/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price', 'inventory', 'last_update']
+    list_editable = ['unit_price', 'inventory']
+    list_per_page = 10
+    # list_filter = ['last_update']
+    # search_fields = ['title']
+
+
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
